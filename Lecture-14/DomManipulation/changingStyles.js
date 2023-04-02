363c2760f4f3f798ad868d36ea5e551695ac9b2f
Yash Atrey <atreyyash@gmail.com>
2023-04-02 15:13:05 +0530

    lec-14

--- Lecture-14/DomManipulation/changingStyles.js
+++ Lecture-14/DomManipulation/changingStyles.js
@@ -5,3 +5,13 @@ console.log(body);
 const ul = document.querySelector('ul');
 
 console.log(ul);
+
+console.log(ul.classList);
+// ClassList mei class: add, delete
+ul.classList.add("meriList");
+
+
+setInterval(() => {
+    // ul.classList.toggle('meriList');
+    // body.classList.toggle('meriBody');
+}, 100);
