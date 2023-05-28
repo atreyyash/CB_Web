4c80418b1fb8228a1c0538c0bc479f7e6fc9c705
Yash Atrey <atreyyash@gmail.com>
2023-05-28 15:30:59 +0530

    Mongoose

---
+++ Lecture-28/app.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = 4444;
+const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+const Users = require('./models/users');
+const Blogs = require('./models/blogs');
+
+app.use((req, res, next) => {
+    Users.find({ _id: "64731c9086a534291b9bd337" })
+        .then(user => {
+            console.log(user[0]);
+            req.user = user[0];
+            next();
+        })
+})
+
+app.get('/myblogs', (req, res) => {
+    console.log("Current Logged In User: ", req.user);
+    Blogs.find({author_id: req.user._id})
+        .populate('author_id')
+        .then((blogs) => {
+            console.log(blogs);
+            res.render('myblogs');
+        })
+})
+
+app.get('/', (req, res) => {
+    console.log("Current Logged In User: ", req.user);
+    Users.find({})
+        .then((users) => {
+            console.log(users);
+            res.render('users', {
+                users
+            });
+        })
+        .catch(err => {
+            res.send("Users nahi mille");
+        })
+
+})
+
+app.post('/adduser', (req, res) => {
+    const { name, age } = req.body;
+
+    let newUser = new Users({ name, age });
+    newUser.save()
+        .then(() => {
+            // res.send("User added Success");
+            res.redirect('/');
+        })
+        .catch(err => {
+            res.send(err);
+        })
+
+})
+
+app.get('/deleteuser', (req, res, next) => {
+    const { id } = req.query;
+
+    Users.deleteOne({ _id: id }).then(() => {
+        res.redirect('/');
+    }).catch(err => {
+        res.send(err);
+    })
+
+})
+
+app.post('/addblog', (req, res, next) => {
+    const { title, description } = req.body;
+    const newBlog = new Blogs({
+        title,
+        description,
+        author_id: req.user._id
+    });
+    newBlog.save()
+        .then(() => {
+            res.redirect('/myblogs');
+        })
+        .catch((err) => {
+            res.send(err);
+        })
+});
+
+mongoose.connect('mongodb://localhost:27017')
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`http://localhost:` + PORT);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    })
---
+++ Lecture-28/models/users.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: String,
+    age: Number,
+    blogId: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'blogs'
+        }
+    ]
+});
+
+module.exports = mongoose.model('users', userSchema);
